1c962944b75fca4f630228c6059580d6082a641d
Steve <stshryu@gmail.com>
2025-01-30 11:58:11 -0500

    Name label for upgrades

--- vampiresurvivortemplate/upgrade.gd
+++ vampiresurvivortemplate/upgrade.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
+@export var upgrade_name: String
 @export var upgrade_label: Label
 @export var sprite: Sprite2D
 @export var ammo_strategy : BaseAmmoStrategy
 
 func _ready() -> void:
 	body_entered.connect(on_body_entered)
-	upgrade_label.text = ammo_strategy.name
+	upgrade_label.text = upgrade_name
 
 func on_body_entered(body: PhysicsBody2D):
 	if body is Player:
