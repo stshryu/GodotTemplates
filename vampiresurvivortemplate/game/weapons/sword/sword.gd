56fe407f40a6cd5aa5a8541f6d23838ea25f70c4
Steve <stshryu@gmail.com>
2025-02-12 19:42:44 -0500

    directory fucked up

---
+++ vampiresurvivortemplate/game/weapons/sword/sword.gd
@@ -0,0 +1,18 @@
+class_name BaseSword
+extends BaseWeapon
+	
+func _ready():
+	ammo = preload("res://game/Ammo/SwordAmmo/SwordAmmo.tscn")
+	projectiles += 3
+	base_projectile_arc_degree = 360.0
+
+func child_shoot_override(ammo: Ammo):
+	ammo.global_rotation += deg_to_rad(-90)
+	return ammo
+	
+func deferred_child_shoot_override(ammo: Ammo):
+	ammo.sprite.rotation_degrees += 90
+	
+func _physics_process(delta):
+	#manual_aim()
+	auto_aim()
