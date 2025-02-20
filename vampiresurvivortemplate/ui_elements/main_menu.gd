6ff6124c5d6d31358225437e1f3f8af975335eef
Steve <stshryu@gmail.com>
2025-02-19 23:42:50 -0500

    adding main menu and linking up menu

--- vampiresurvivortemplate/ui_elements/main_menu.gd
+++ vampiresurvivortemplate/ui_elements/main_menu.gd
@@ -7,4 +7,15 @@ extends Control
 @export var initial_scene: PackedScene = preload("res://game/game.tscn")
 
 func _ready():
-	pass
+	start_button.button_up.connect(_start_level)
+	option_button.button_up.connect(_open_options)
+	exit_button.button_up.connect(_exit_game)
+
+func _start_level():
+	get_tree().change_scene_to_packed(initial_scene)
+	
+func _open_options():
+	print('open options')
+	
+func _exit_game():
+	get_tree().quit()
