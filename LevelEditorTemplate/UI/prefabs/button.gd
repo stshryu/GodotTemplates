2716608f07b5bd6acb043487e4bd0ce51e67d1ce
Steve <stshryu@gmail.com>
2025-03-07 10:29:52 -0500

    Adding button prefab logic

--- LevelEditorTemplate/UI/prefabs/button.gd
+++ LevelEditorTemplate/UI/prefabs/button.gd
@@ -2,8 +2,16 @@ extends TextureButton
 
 @onready var label: Label = %ButtonLabel
 @export var dimensions: Vector2
-@export var label_name: String
+@export var label_name: String = "placeholder"
+
+var button_state 
 
 func _ready():
 	self.custom_minimum_size = dimensions
 	label.text = label_name
+
+func _on_button_down():
+	label.position.y += 2
+
+func _on_button_up():
+	label.position.y -= 2
