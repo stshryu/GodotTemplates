166556fa3651c0695f8d6cb17b42950ea2c744cf
Steve <stshryu@gmail.com>
2025-03-09 23:21:17 -0400

    Adding a texture atlas, that selects sprites to insert into the scene when needed

--- LevelEditorTemplate/UI/level_editor/level_editor.gd
+++ LevelEditorTemplate/UI/level_editor/level_editor.gd
@@ -5,15 +5,19 @@ extends Control
 @onready var right_control_panel = %RightControlPanel
 @onready var right_panel_timer = %PanelTimeout
 @onready var right_button_container = %VBoxRightContainer
+@onready var editor_node = %EditorNode
 
 var viewport_dimensions := Vector2.ZERO
 var right_panel_timeout := false
 var opening_right_panel := false
 var closing_right_panel := false
-var prefab_button := preload("res://UI/prefabs/button.tscn")
-var button_texture_atlas_key: Dictionary[TextureButton, Array] = {}
+var is_editing := false
 
-signal buttonpressed
+var prefab_button := preload("res://UI/prefabs/button.tscn")
+var atlas_texture := preload("res://assets/Modern_Office_48x48.png")
+var button_texture_atlas_key: Dictionary[TextureButton, Array] = {}
+var currently_active_placement: AtlasTexture
+var preview: Sprite2D
 
 func add_button(button_name: String, button_dimensions: Vector2, button_atlas_coords: Vector2, button_atlas_dimension: Vector2):
 	right_button_container.add_child(HSeparator.new())
@@ -28,20 +32,37 @@ func add_button(button_name: String, button_dimensions: Vector2, button_atlas_co
 
 func _ready():
 	viewport_dimensions = get_viewport_rect().size
-	add_button("test", Vector2(60.0,20.0), Vector2(10.0, 10.0), Vector2(15.0, 15.0))
-	add_button("test2", Vector2(60.0,20.0), Vector2(10.0, 10.0), Vector2(15.0, 15.0))
 	
-func _physics_process(delta):
+	add_button("Printer", Vector2(60.0,20.0), Vector2(393.0, 990.0), Vector2(75.0, 60.0))
+	add_button("Computer", Vector2(60.0,20.0), Vector2(672.0, 630.0), Vector2(48.0, 66.0))
+	
+func _physics_process(_delta):
 	if not right_panel_timeout:
 		display_right_panel()
 	if opening_right_panel:
-		open_right_panel(delta)
+		open_right_panel()
 	elif closing_right_panel:
-		close_right_panel(delta)
+		close_right_panel()
+		
+	if is_editing:
+		var mouse_pos := get_local_mouse_position()
+		preview.position = mouse_pos
+		
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			preview.modulate.a = 1.0
+			is_editing = false 
 
 func _on_right_panel_button_pressed(pressed_button):
-	print(pressed_button)
-	print(button_texture_atlas_key[pressed_button])
+	is_editing = true
+	currently_active_placement = AtlasTexture.new()
+	currently_active_placement.set_atlas(atlas_texture)
+	currently_active_placement.region = Rect2(
+		button_texture_atlas_key[pressed_button][0], button_texture_atlas_key[pressed_button][1]
+	)
+	preview = Sprite2D.new()
+	preview.texture = currently_active_placement
+	preview.modulate.a = 0.5
+	editor_node.call_deferred("add_child", preview)
 	
 func display_right_panel():
 	var x_mouse_pos = get_viewport().get_mouse_position()[0]
@@ -49,12 +70,12 @@ func display_right_panel():
 		right_control_panel.visible = true
 		opening_right_panel = true
 		
-func open_right_panel(delta):
+func open_right_panel():
 	right_control_panel.position.x -= 5
 	if right_control_panel.position.x == viewport_dimensions[0] - 60:
 		opening_right_panel = false
 
-func close_right_panel(delta):
+func close_right_panel():
 	right_control_panel.position.x += 5
 	if right_control_panel.position.x == viewport_dimensions[0]:
 		right_control_panel.visible = false
